a3c1eef87efab0eac9569d65da74e84292519e98
Faris <mfarismazley96@gmail.com>
2025-01-24 18:49:15 +0800

    Setup redux, finish sidebar with links, light and dark mode setup.

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,27 +1,129 @@
 "use client";
-import { Menu } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setSidebarCollapsed } from "@/state";
+import { Archive, CircleDollarSign, Clipboard, Layout, LucideIcon, Menu, SlidersHorizontal, User } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
-const Sidebar = () => {
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isAvtice =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
   return (
-    <div>
+    <Link href={href}>
+      <div
+        className={`cursor-pointer flex items-center ${
+          isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
+        } hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
+          isAvtice ? "bg-blue-200 text-white" : ""
+        }`}
+      >
+        <Icon className="w-6 h-6 !text-gray-700"></Icon>
+        <span
+          className={`${
+            isCollapsed ? "hidden" : "block"
+          } font-medium text-gray-700`}
+        >
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
+};
+
+const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSideBarCollapsed
+  );
+  const toggleSideBar = () => {
+    dispatch(setSidebarCollapsed(!isSidebarCollapsed));
+  };
+  const sideBarClassNames = `fixed flex-col ${
+    isSidebarCollapsed ? "hidden md:w-16 md:flex" : "w-72 md:w-64 flex"
+  } bg-white transition-all duration-300 h-full shadow-md z-40`;
+
+  return (
+    <div className={sideBarClassNames}>
       {/* TOP LOGO */}
-      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+      <div
+        className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+          isSidebarCollapsed ? "px-5" : "px-8"
+        }`}
+      >
         <div>logo</div>
-        <h1 className="font-extrabold text-2xl">ELECTROL</h1>
+        <h1
+          className={`${
+            isSidebarCollapsed ? "hidden" : "block"
+          } font-extrabold text-2xl 
+          `}
+        >
+          ELECTROL
+        </h1>
         <button
           className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSideBar}
         >
           <Menu className="w-4 h-4" />
         </button>
       </div>
 
       {/* LINKS */}
-      <div className="bg-gray-300 flex-grow mt-8">{/* links here */}links</div>
+      <div className="flex-grow mt-8">
+        <SidebarLink
+          href="/dashboard"
+          icon={Layout}
+          label="Dashboard"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/inventory"
+          icon={Archive}
+          label="Inventory"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/products"
+          icon={Clipboard}
+          label="Product"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/users"
+          icon={User}
+          label="Users"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/settings"
+          icon={SlidersHorizontal}
+          label="Settings"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/expenses"
+          icon={CircleDollarSign}
+          label="Expenses"
+          isCollapsed={isSidebarCollapsed}
+        />
+      </div>
 
       {/* FOOTER */}
-      <div>
+      <div className={`${isSidebarCollapsed? "hidden":"block"} mb-10`}>
         <p className="text-center text-xs text-gray-500">
           &copy; 2025 Electrol
         </p>
--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -13,20 +13,24 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   useEffect(() => {
-    if(isDarkMode){
-      document.documentElement.classList.add("dark")
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.add("light")
+      document.documentElement.classList.add("light");
     }
-  })
+  });
 
   return (
-    <div className='${isDarkMode ? "dark" : "light"} flex bg-gray-50 text-gray-900 w-full min-h-screen'>
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
       <Sidebar />
       <main
-        className={
-          'flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}'
-        }
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        }`}
       >
         <Navbar />
         {children}

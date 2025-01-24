b3cf94994da293d061d0ee53a279293703840130
Faris <mfarismazley96@gmail.com>
2025-01-24 20:49:54 +0800

    Setup dashboard routes and controllers. Test API call from db

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,23 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 /* ROUTE IMPORTS*/
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 /* CONFIRMATIONS */
 dotenv.config(); 
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+
+/* SERVER */
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
